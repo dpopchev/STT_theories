11cfe154aa9290cf3412c39a561e9a5cad1fccbc
dpopchev <dpopchev@gmail.com>
2017-10-19 11:30:22 +0300

    better strutcture - bin, build, headers, src directories; makefile done to work with them

---
+++ headers/ExternalHeaders.h
@@ -0,0 +1,11 @@
+#ifndef _EXTERNAL_HEADERS_H_
+    #define _EXTERNAL_HEADERS_H_
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
+    #include <math.h>
+    #include <time.h>
+    #include <unistd.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include <ExternalHeaders.h>
+
+int main() {
+    printf("dsadsa , world!\n");
+    return 0;
+}
