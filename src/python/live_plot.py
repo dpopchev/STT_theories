16cc7ec27f4a00561135fdb32a70a85d5bd7f994
dpopchev <dpopchev@gmail.com>
2018-01-13 13:41:31 +0200

    live_plot.py working as I want each new set of points tarts with "#" and what follows will be used for title for the set of points to follow it searches for the file live_plot_data inside resurces/ folder

--- src/python/live_plot.py
+++ src/python/live_plot.py
@@ -11,14 +11,18 @@ def animate(i):
     with open(file_path + file_name,'r') as f:
         graph_data = f.read()
 
+    graph_data = [ k.split("\n") for k in graph_data.split("# ") if len(k) > 1 ]
+
     plot_x, plot_y = \
-        [ [] for k in range(0,len(graph_data.split("\n\n"))) ], \
-        [ [] for k in range(0,len(graph_data.split("\n\n"))) ]
+        [ [] for k in range(0,len(graph_data)) ], \
+        [ [] for k in range(0,len(graph_data)) ]
+
+    plot_titles = [ k.pop(0) for k in graph_data ]
 
     for single_set, single_set_x, single_set_y \
-      in zip(graph_data.split("\n\n"),plot_x,plot_y):
+      in zip(graph_data, plot_x, plot_y):
 
-        for single_line in single_set.split("\n"):
+        for single_line in single_set:
             if len(single_line) > 1:
                 x,y = single_line.split(",")
                 single_set_x.append(float(x))
@@ -28,16 +32,14 @@ def animate(i):
     ax.xaxis.set_major_formatter(FormatStrFormatter('%.3e'))
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.3e'))
 
-    i = 0
-    for single_set_x, single_set_y in zip(plot_x, plot_y):
+    for single_set_x, single_set_y , single_set_title \
+      in zip(plot_x, plot_y, plot_titles):
         ax.plot(\
           single_set_x, single_set_y, \
           marker="o", markersize=7, \
           linewidth=1.5, \
-          label=str(i) \
+          label=single_set_title \
         )
-
-        i+=1
 
     ax.legend(loc="best")
 
