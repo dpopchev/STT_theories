f693f16b1cf017b9a8137451e8d4fe7639ea9dc1
dpopchev <dpopchev@gmail.com>
2018-01-13 13:22:56 +0200

    live_plto.py working as expected, about to add titling

--- src/python/live_plot.py
+++ src/python/live_plot.py
@@ -12,37 +12,49 @@ def animate(i):
         graph_data = f.read()
 
     plot_x, plot_y = \
-        [ [] for i in range(0,len(graph_data.split("\n\n"))) ], \
-        [ [] for i in range(0,len(graph_data.split("\n\n"))) ]
+        [ [] for k in range(0,len(graph_data.split("\n\n"))) ], \
+        [ [] for k in range(0,len(graph_data.split("\n\n"))) ]
 
     for single_set, single_set_x, single_set_y \
       in zip(graph_data.split("\n\n"),plot_x,plot_y):
 
-        for i in single_set.split("\n"):
-            if len(i) > 1:
-                x,y = i.split(",")
-                single_set_x.append(x)
-                single_set_y.append(y)
+        for single_line in single_set.split("\n"):
+            if len(single_line) > 1:
+                x,y = single_line.split(",")
+                single_set_x.append(float(x))
+                single_set_y.append(float(y))
 
-    ax1.clear()
-    for i, j in zip(plot_x, plot_y):
-        ax1.plot(i,j,marker="o")
+    ax.clear()
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%.3e'))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.3e'))
+
+    i = 0
+    for single_set_x, single_set_y in zip(plot_x, plot_y):
+        ax.plot(\
+          single_set_x, single_set_y, \
+          marker="o", markersize=7, \
+          linewidth=1.5, \
+          label=str(i) \
+        )
+
+        i+=1
+
+    ax.legend(loc="best")
 
 if __name__ == "__main__":
 
-    import matplotlib.pyplot as plt
-    import matplotlib.animation as animation
-
+    from matplotlib import pyplot as plt
+    from matplotlib import animation as animation
     from matplotlib import style
     from matplotlib.ticker import FormatStrFormatter
 
-    #style.use("seaborn-poster")
-    style.use("fivethirtyeight")
+    style.use("seaborn-poster")
+    #style.use("fivethirtyeight")
 
     fig = plt.figure()
-    ax1 = fig.add_subplot(1,1,1)
-    ax1.xaxis.set_major_formatter(FormatStrFormatter('%.3e'))
-    ax1.yaxis.set_major_formatter(FormatStrFormatter('%.3e'))
+    ax = fig.add_subplot(1,1,1)
+    ax.xaxis.set_major_formatter(FormatStrFormatter('%.3e'))
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.3e'))
 
     ani = animation.FuncAnimation(fig,animate,interval=1000)
     plt.show()
